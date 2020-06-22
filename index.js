fedcaa76cf76dbeaaf2319910500c368b372e153
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-06-22 13:54:38 +1000

    feat: Update for new blog

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 const Xray = require('x-ray');
 
-/**
- * ! NOT COMPLETE
- * Return blog posts from https://dennisokeeffe.com/blog
- * @return {Promise} [posts]
- */
-function getPosts(url = 'https://dennisokeeffe.com/blog') {
-    const x = Xray();
-    return new Promise((resolve, reject) => {
-        x(`${url}`, '.component-ga-tagged-list-one div.item.four', [
-            {
-                title: 'h3.text.bold',
-                description: '.text.description',
-                tags: ['.text.link.tag'],
-                image: 'a.text.link@href'
-            }
-        ])((err, data) => {
-            if (err) {
-                reject(err);
-            }
-            // console.log(data);
-            resolve(data);
-        });
+function getPosts(url = 'https://blog.dennisokeeffe.com/') {
+  const x = Xray();
+  return new Promise((resolve, reject) => {
+    x(`${url}`, 'main:last-child', {
+      items: x('div', [
+        {
+          title: 'h3 > a',
+          description: 'p',
+          link: 'h3 > a@href',
+          date: 'small',
+        },
+      ]),
+    })((err, data) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(data);
     });
+  });
 }
 
-/**
- * Function to run from script.
- *
- */
-const run = async() => {
-    const posts = await getPosts();
-    console.log(posts);
-}
+const main = async () => {
+  const posts = await getPosts();
+  console.log(posts);
+};
 
-run();
+main();
